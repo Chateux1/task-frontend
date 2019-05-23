145f86e588974f9f3f5811ecffac999e0d60791f
Chateux1 <m.satas@gmail.com>
2019-05-23 18:23:54 +0300

    Test style refactoring
    - Allow usage of global variables in component style files
    - Allow to rewrite class names in standard "" format instead of {}
    - Use red color from variable in footer text as a test.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,27 +1,24 @@
 import React from "react";
-import styles from "../../styles/components/footer.scss";
 
 const Footer = () => (
   <footer className="page-footer">
-    {/* <div className="container"> */}
-    <div className={styles.contentFooter}>
-      <a className={styles.menuItem1}>
+    <div className="contentFooter">
+      <a className="menuItem1">
         <i className="fas fa-check" />
         <span> Guaranteed 99.9% Uptime</span>
       </a>
-      <a className={styles.menuItem2}>
+      <a className="menuItem2">
         <i className="fas fa-check" />
         <span> Superior Speed Performance</span>
       </a>
-      <a className={styles.menuItem3}>
+      <a className="menuItem3">
         <i className="fas fa-check" />
         <span> 24/7 Support Chat</span>
       </a>
-      <a className={styles.menuItem4}>
+      <a className="menuItem4">
         <i className="fas fa-check" />
         <span> 30-Day Money-Back Guarantee</span>
       </a>
-      {/* </div> */}
     </div>
   </footer>
 );
