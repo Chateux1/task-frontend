ae19d2d4415a68932a651d2de281bc59c4129707
Chateux1 <m.satas@gmail.com>
2019-05-27 00:18:10 +0300

    Make background image size responsive - Create different resolution files of the same image manually - Use eventlisteners to change file according to screen size -- Had to use containers again because parent styling is not available
    Other changes;
    - Remove header margin that was breaking the new layout
    - Remove hash part from image naming in webpack setup

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,69 @@
 import { hot } from "react-hot-loader/root";
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+
 import Header from "../header";
 import Footer from "../footer";
-import Offer from "../offer";
-import Price from "../price";
+
+import bgHD from "../../../assets/bg-hd.jpg";
+import bgExtraLarge from "../../../assets/bg-extra-large.jpg";
+import bgLarge from "../../../assets/bg-large.jpg";
+import bgMedium from "../../../assets/bg-medium.jpg";
+import bgSmall from "../../../assets/bg-small.jpg";
+import bgMobile from "../../../assets/bg-mobile.jpg";
 
 import "./app.scss";
 import "../../index.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        {/* <Offer />
-        <Price /> */}
-        <Footer />
-      </>
-    );
+const App = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  let imageUrl = bgMobile;
+
+  switch (true) {
+    case window.innerWidth >= 2000:
+      imageUrl = bgHD;
+      break;
+    case window.innerWidth >= 1500 && window.innerWidth < 2000:
+      imageUrl = bgExtraLarge;
+      break;
+    case window.innerWidth >= 1200 && window.innerWidth < 1500:
+      imageUrl = bgLarge;
+      break;
+    case window.innerWidth >= 900 && window.innerWidth < 1200:
+      imageUrl = bgMedium;
+      break;
+    case window.innerWidth >= 600 && window.innerWidth < 900:
+      imageUrl = bgSmall;
+      break;
+    case window.innerWidth < 600:
+      imageUrl = bgMobile;
+      break;
   }
-}
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
+
+  return (
+    <div
+      className="page-container"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    >
+      <div className="page-wrapper">
+        <Header />
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default hot(App);
--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
             loader: "url-loader",
             options: {
               limit: 4000,
-              name: "images/[hash]-[name].[ext]"
+              name: "images/[name].[ext]"
             }
           }
         ]
